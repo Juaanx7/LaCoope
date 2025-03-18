4ba77dc43ae9739aefa6c3c509f463d40a4cd5b5
Juaanx7 <juaanx7@gmail.com>
2025-03-17 22:15:26 -0300

    implementacion de agregar y eliminar cleintes y otras correcciones

---
+++ src/pages/Configuracion.jsx
@@ -0,0 +1,11 @@
+function Configuracion() {
+    return (
+      <div>
+        <h1>Página de Configuración</h1>
+        <p>Aquí podrás cambiar ajustes del sistema.</p>
+      </div>
+    );
+  }
+  
+  export default Configuracion;
+  
